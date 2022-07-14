4b416e8b86534e5edd622e86b4beadc571bd15f2
whp <example>
2022-07-14 03:04:06 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const btn = document.getElementById("btn");
 const advice = document.getElementById("advice");
+const adviceId = document.getElementById("adviceId");
 
 btn.addEventListener("click", () => {
-  console.log("test");
+  fetch("https://api.adviceslip.com/advice").then((response) => {
+    const res = response.json();
+    res.then((res) => {
+      advice.innerText = `${res.slip.advice}`;
+      adviceId.innerText = `${res.slip.id}`;
+    });
+  });
 });
